bfbf1f3baeeba30b3c59295a8d21d66df6f09cec
danascape <danascape@gmail.com>
2023-11-12 10:29:24 +0100

    spartan: Drop atrace HAL
    It is deprecated in Android 14.
    
    Change-Id: I5af8056853058ad29be7debb5c5af5dea83d84a9

--- device.mk
+++ device.mk
@@ -34,10 +34,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PACKAGES += \
     AntHalService-Soong
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
