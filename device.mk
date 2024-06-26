a6b4bcacdbdf44de123a18a8f62c5b2759d276d0
ryukftw <workforbijoy9@gmail.com>
2024-06-26 17:26:20 +0000

    Revert "spartan: nuke oplus doze"
    This reverts commit e47168da6f7fda102d7ddcf641e3d10cdab4e05a.

--- device.mk
+++ device.mk
@@ -194,6 +194,10 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v33/arm/arch-arm-armv7-a-neon/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33.so \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
 
+# Doze
+PRODUCT_PACKAGES += \
+    OplusDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
@@ -365,7 +369,9 @@ PRODUCT_PACKAGES += \
     WifiResTarget
 
 # Oplus Camera
+ifeq ($(WITH_OPLUS_CAMERA),true) # use export WITH_OPLUS_CAMERA=true after lunch to build camera
 $(call inherit-product, vendor/oplus/camera/opluscamera.mk)
+endif
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
