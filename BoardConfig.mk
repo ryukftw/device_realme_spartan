aa551f7314f96ed711a086ec4b5ba4d35f900784
LuK1337 <priv.luk@gmail.com>
2024-08-27 00:00:55 +0200

    spartan: Switch to common device_framework_matrix.xml
    Change-Id: Idc99a59a377bb3284634a62c0a1ae545bcd4e1e2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,7 +84,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/device_framework_matrix.xml \
+    hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
