10db6c386310964785b1a9236dba5559ffcd6c2c
ryukftw <workforbijoy9@gmail.com>
2024-12-16 15:56:54 +0000

    spartan: Switch to AIDL sensors multihal
    Signed-off-by: ryukftw <workforbijoy9@gmail.com>

--- device.mk
+++ device.mk
@@ -276,7 +276,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors-service.oplus-multihal \
     sensors.oplus
 
 PRODUCT_COPY_FILES += \
