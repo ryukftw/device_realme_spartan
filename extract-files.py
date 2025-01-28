a22f25f90c7025644ca8ba70a3b55cb62c2e2e00
ryukftw <workforbijoy9@gmail.com>
2025-01-28 13:00:17 +0000

    spartan: Patch camera blobs to use libstdc++_vendor
    Fixes:
    error: vendor/realme/spartan/Android.bp:20943:1: dependency "libstdc++" of "libYTCommon" missing variant:
      os:android,image:vendor,link:shared
    available variants:
      os:linux_glibc,arch:x86_64,link:static
      os:linux_glibc,arch:x86_64,link:shared
      os:linux_glibc,arch:x86,link:static
      os:linux_glibc,arch:x86,link:shared
      os:windows,arch:x86,link:static
      os:windows,arch:x86,link:shared
      os:windows,arch:x86_64,link:static
      os:windows,arch:x86_64,link:shared
      os:android,arch:arm64_armv8-a,link:static
      os:android,arch:arm64_armv8-a,link:shared
      os:android,arch:arm_armv8-a,link:static
      os:android,arch:arm_armv8-a,link:shared
      os:android,image:ramdisk,arch:arm64_armv8-a,link:static
      os:android,image:ramdisk,arch:arm64_armv8-a,link:shared
      os:android,image:vendor_ramdisk,arch:arm64_armv8-a,link:static
      os:android,image:vendor_ramdisk,arch:arm64_armv8-a,link:shared
      os:android,image:recovery,arch:arm64_armv8-a,link:static
      os:android,image:recovery,arch:arm64_armv8-a,link:shared
    
    Change-Id: Id09700450f57abcea903a119f36800ff7fd88090

--- extract-files.py
+++ extract-files.py
@@ -92,6 +92,8 @@ def blob_fixup_nop_call(
 blob_fixups: blob_fixups_user_type = {
     'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service': blob_fixup()
         .add_needed('libshims_fingerprint.oplus.so'),
+    ('odm/lib64/libCOppLceTonemapAPI.so', 'odm/lib64/libYTCommon.so', 'odm/lib64/libaps_frame_registration.so'): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     ('odm/lib64/mediadrm/libwvdrmengine.so', 'odm/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
     'product/app/PowerOffAlarm/PowerOffAlarm.apk': blob_fixup()
