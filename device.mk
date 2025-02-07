bc23eea78aaea1bd98869ab7ebdfba21a7d60d30
Michael Bestas <mkbestas@lineageos.org>
2025-02-07 04:54:46 +0200

    spartan: Switch to audio_amplifier.qcom
    Change-Id: Ice61834935fd7249d13dadff476bab0dda04df1d

--- device.mk
+++ device.mk
@@ -45,7 +45,7 @@ PRODUCT_PACKAGES += \
     audio.primary.kona \
     audio.r_submix.default \
     audio.usb.default \
-    audio_amplifier.kona \
+    audio_amplifier.qcom \
     liba2dpoffload \
     libbatterylistener \
     libcomprcapture \
