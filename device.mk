6def2a96844b84b9fc3e23dafeab0923fff9e157
Michael Bestas <mkbestas@lineageos.org>
2025-02-28 01:21:09 +0200

    lemonades: Enable UFFD GC
    Kernel has support for it now.
    
    Change-Id: I3863ee71477f321fe47646571b857fdc39a1902b

--- device.mk
+++ device.mk
@@ -220,6 +220,9 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_oplus)
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
