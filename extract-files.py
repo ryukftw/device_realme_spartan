ProjectEverest-Devices/android_device_realme_spartan

spartan: Patch libAlgoProcess to resolve duplicate dependencies
Fixes:
error: module "libAlgoProcess" variant
"android_vendor_arm64_armv9-a_static": depends on multiple
versions of the same aidl_interface:
android.hardware.graphics.common-V3-ndk-source,
android.hardware.graphics.common-V5-ndk-source

Change-Id: If0be3da5c13e45c6c59dde98bb80f35eb22bf846

extract-files.py
@@ -85,6 +85,8 @@ blob_fixups: blob_fixups_user_type = {
         .sig_replace('23 0A 00 94', '1F 20 03 D5'),
     'odm/lib/libdlbdsservice_v3_6.so|odm/lib/libstagefright_soft_ddpdec.so|odm/lib/libstagefrightdolby.so|odm/lib64/libdlbdsservice_v3_6.so': blob_fixup()
         .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
+    'odm/lib64/libAlgoProcess.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V5-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(

ryukftw/device_realme_spartan

extract-files.py
@@ -120,6 +120,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
         .add_needed('libcamera_metadata_shim.so')
         .binary_regex_replace(b'com.oem.autotest', b'\x00om.oem.autotest'),
+    'odm/lib64/libAlgoProcess.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V6-ndk.so'),
     'odm/lib64/libOGLManager.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')